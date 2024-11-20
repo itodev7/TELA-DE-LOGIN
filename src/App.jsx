54a8c4ed794af0f1263b1e230bbe324292a86909
itodev7 <ito7dev@gmail.com>
2024-11-19 21:19:09 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+// import Login from './Components/login/Login';
+import Registro from './Components/registro/Registro'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Registro/>
+    </div>
+  );
+}
+
+export default App
